bdeaa7a8353a3b686c77ca6e8c69a2d8d89585bc
Thomas <tmalidin33@gmail.com>
2019-03-17 13:05:46 +0100

    Update mise en forme de la liste des utilisateurs et des profils -> Boostrap

--- templates/headTemplate.php
+++ templates/headTemplate.php
@@ -3,10 +3,13 @@
 <head>
 	<meta charset="utf-8">
 	<title>PROJET WEB</title>
-	<link rel="stylesheet" href="css/generalStyle.css" type="text/css" 
+	<link rel="stylesheet" href="css/generalStyle.css" type="text/css"
 	media="screen" title="style1" charset="utf-8" />
 	<script src="js/code.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 </head>
 <body>
 	<div id="page">
-		
--- templates/usersTemplate.php
+++ templates/usersTemplate.php
@@ -1,4 +1,42 @@
-<?php $users = $args['users'];
-foreach ($users as $user) {
-	require('userTemplate.php');
-} ?>
+<div class="container">
+  <h2>Listes de utilisateurs</h2>
+  <table class="table table-hover">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Prénom</th>
+        <th>Nom</th>
+				<th>Login</th>
+        <th>Mot de passe</th>
+				<th>Email</th>
+        <th>Role</th>
+				<th>Promo</th>
+        <th>Groupe</th>
+        <th>TD</th>
+				<th>Matricule</th>
+        <th>Interne/Externe</th>
+        <th>Matière enseignée</th>
+      </tr>
+    </thead>
+    <tbody>
+			<?php $users = $args['users'];
+			foreach ($users as $user) {
+				echo "<tr>";
+				echo "<td>". $user->id(). "</td>";
+				echo "<td>". $user->prenom(). "</td>";
+				echo "<td>". $user->nom(). "</td>";
+				echo "<td>". $user->login(). "</td>";
+				echo "<td>". $user->mdp(). "</td>";
+				echo "<td>". $user->mail(). "</td>";
+				echo "<td>". $user->role(). "</td>";
+				echo "<td>". $user->promo(). "</td>";
+				echo "<td>". $user->td(). "</td>";
+				echo "<td>". $user->grp_demiPromo(). "</td>";
+				echo "<td>". $user->matricule(). "</td>";
+				echo "<td>". $user->int_ext(). "</td>";
+				echo "<td>". $user->matiere(). "</td>";
+				echo "</tr>";
+			} ?>
+    </tbody>
+  </table>
+</div>
