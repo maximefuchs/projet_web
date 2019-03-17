6d0e3e9e1051be094deb37502b70a9c650a19d49
Thomas <tmalidin33@gmail.com>
2019-03-17 23:04:51 +0100

    collapse réponses

--- templates/questionEtreponse.php
+++ templates/questionEtreponse.php
@@ -1,10 +1,16 @@
 <?php
-echo "<br>";
-echo $q->description_question();
-echo "<br>";
+echo " <div class=\"panel panel-default\">
+<div class=\"panel-heading\">
+<h4 class=\"panel-title\">
+<a data-toggle=\"collapse\" data-parent=\"#accordion\" href=\"#collapse".$compteur."\">".$q->description_question()."</a>
+</h4>
+</div>
+<div id=\"collapse".$compteur."\" class=\"panel-collapse collapse \"><div class=\"panel-body\">";
 foreach ($rep_associee as $r) {
 	echo $r->contenu();
 	echo "<br>";
 }
-
- ?>
+echo"</div>
+</div>
+</div>";
+?>
--- templates/questionsEtreponsesTemplate.php
+++ templates/questionsEtreponsesTemplate.php
@@ -1,17 +1,21 @@
-<?php 
-
-$question = $this->args['questions'];
-$reponses = $this->args['reponses'];
-
-foreach ($question as $q) {
-	$q_id = $q->id();
-	$rep_associee = array();
-	foreach ($reponses as $r) {
-		if($r->idQuestion() == $q_id){
-			array_push($rep_associee, $r);
+<div class="container">
+	<h2>Questions et réponses</h2>
+	<div class="panel-group" id="accordion">
+		<?php
+		$question = $this->args['questions'];
+		$reponses = $this->args['reponses'];
+		$compteur=1;
+		foreach ($question as $q) {
+			$q_id = $q->id();
+			$rep_associee = array();
+			foreach ($reponses as $r) {
+				if($r->idQuestion() == $q_id){
+					array_push($rep_associee, $r);
+				}
+			}
+			require('questionEtreponse.php');
+			$compteur=$compteur+1;
 		}
-	}
-	require('questionEtreponse.php');
-}
-
- ?>
+		?>
+	</div>
+</div>
