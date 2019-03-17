2b9cd111125121d39c7c066796527553a682887e
Thomas <tmalidin33@gmail.com>
2019-03-17 19:53:33 +0100

    CSS Boostrap réponses

--- templates/reponsesTemplate.php
+++ templates/reponsesTemplate.php
@@ -1,5 +1,34 @@
-<?php $reponses = $args['reponses'];
-foreach ($reponses as $r) {
-	require('reponseTemplate.php');
-}
-?>
+<div class="container">
+  <h2>Liste des réponses</h2>
+	<p><span class="glyphicon glyphicon-search"></span>Rechercher parmis les réponses</p>
+  <input class="form-control" id="myInput" type="text" placeholder="Recherche..">
+  <br>
+  <table class="table table-hover">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Contenu</th>
+        <th>Est Juste</th>
+				<th>Colonne</th>
+        <th>Question associée</th>
+      </tr>
+    </thead>
+    <tbody id="myTable">
+			<?php $reponses = $args['reponses'];
+			foreach ($reponses as $r) {
+				require('reponseTemplate.php');
+			}
+			?>
+    </tbody>
+  </table>
+</div>
+<script>
+$(document).ready(function(){
+  $("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#myTable tr").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
+</script>
