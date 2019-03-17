5241f8ea9daa4e85b904ac044c084b0381ed48dd
Maxime <maxime.fuchs1@gmail.com>
2019-03-17 20:42:56 +0100

    types des input dans formulaire

--- templates/inscriptionTemplate.php
+++ templates/inscriptionTemplate.php
@@ -8,23 +8,23 @@ if(isset($args['inscErrorText']))
 		<tr>
 			<tr>
 				<th>Login* :</th>
-				<td><input type="text" name="inscLogin" required/></td>
+				<td><input type="text" name="inscLogin" required maxlength="50" /></td>
 			</tr>
 			<tr>
 				<th>Mot de passe* :</th>
-				<td><input type="password" name="inscPassword" required/></td>
+				<td><input type="password" name="inscPassword" required minlength="8" maxlength="50" /></td>
 			</tr>
 			<tr>
 				<th>Nom* :</th>
-				<td><input type="text" name="nom" required/></td>
+				<td><input type="text" name="nom" required maxlength="30" /></td>
 			</tr>
 			<tr>
 				<th>Prénom* :</th>
-				<td><input type="text" name="prenom" required/></td>
+				<td><input type="text" name="prenom" required maxlength="30" /></td>
 			</tr>
 			<tr>
 				<th>Mail :</th>
-				<td><input type="text" name="mail" required/></td>
+				<td><input type="text" name="mail" required maxlength="100" /></td>
 			</tr>
 
 			<tr>
@@ -39,27 +39,27 @@ if(isset($args['inscErrorText']))
 
 			<tr class="eleve">
 				<th>Promo :</th>
-				<td><input class="eleveInput" type="text" name="Promo"></td>
+				<td><input class="eleveInput" type="number" name="Promo"></td>
 			</tr>
 
 			<tr class="eleve">
 				<th>Groupe de demi promo :</th>
-				<td><input class="eleveInput" type="text" name="Groupe"></td>
+				<td><input class="eleveInput" type="number" name="Groupe"></td>
 			</tr>
 
 			<tr class="eleve">
 				<th>Td :</th>
-				<td><input class="eleveInput" type="text" name="td"></td>
+				<td><input class="eleveInput" type="number" name="td"></td>
 			</tr>
 
 			<tr class="enseignant">
 				<th>Matricule :</th>
-				<td><input class="enseignantInput" type="text" name="Matricule"></td>
+				<td><input class="enseignantInput" type="number" name="Matricule" maxlength="10"></td>
 			</tr>
 
 			<tr class="enseignant">
 				<th>Matière enseignée :</th>
-				<td><input class="enseignantInput" type="text" name="Mat_enseignee"></td>
+				<td><input class="enseignantInput" type="text" name="Mat_enseignee" maxlength="30"></td>
 			</tr>
 
 			<tr class="enseignant">
