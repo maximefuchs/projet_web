47ae669df1b06cc28f2427cc02417b3f5373cdee
Maxime <maxime.fuchs1@gmail.com>
2019-03-19 18:15:50 +0100

    message welcome dans le bandeau

--- templates/menuTemplate.php
+++ templates/menuTemplate.php
@@ -1,2 +1,2 @@
-<a href="index.php?action=inscription"><li>Incription</li></a>
+<a href="index.php?action=inscription"><li>Inscription</li></a>
 <a href="index.php?action=connexion"><li>Connexion</li></a>
--- templates/userBandeauMenuTemplate.php
+++ templates/userBandeauMenuTemplate.php
@@ -1,4 +1,5 @@
 <span onclick='openNav()'>&#9776; MENU</span>
+<i id="bienvenue">Bonjour <?php echo $this->args['user']->prenom() ?></i>
 <div>
 	<a href= 'index.php'>Home</a>
 	<a href='logout.php'>Deconnexion</a>
