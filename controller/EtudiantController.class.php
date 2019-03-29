8e2a5eba9cc37f1ae32457149b27c2c6ff91bb37
Maxime <maxime.fuchs1@gmail.com>
2019-03-29 18:27:21 +0100

    idem

--- controller/EtudiantController.class.php
+++ controller/EtudiantController.class.php
@@ -5,9 +5,9 @@ class EtudiantController extends UserController{
 	public function __construct($request){
 		parent::__construct($request);
 
-		// if(isset($_POST['ValiderReponses'])){
-		// 	$this->validerReponses($request);
-		// }
+		if(isset($_POST['ValiderReponses'])){
+			$this->validerReponses($request);
+		}
 	}
 
 	public function questionnairesAction($request){
@@ -40,6 +40,10 @@ class EtudiantController extends UserController{
 		// d'abord regrouper les reponses par question puis les traiter
 		// 
 // 
+		$QCM = false;
+		$repQCM= array();
+		$nbQuestions = 0;
+		$compteur = 0;
 		foreach ($_POST as $key => $value) {
 			$explode = explode("_", $key);
 			$type = $explode[0];
@@ -47,10 +51,6 @@ class EtudiantController extends UserController{
 
 			// pour les questions de type différent de QCM on peut savoir directement si la question a été repondue juste dans son intégralité
 			// pour un QCM, il faut récupérer tous les éléments, cochés ou non, pour savoir si l'élève a bien coché toutes les bonnes réponses
-			$QCM = false;
-			$repQCM= array();
-			$nbQuestions;
-			$compteur = 0;
 
 			if($type == 'QCM'){
 				$repQCM[$key] = $value;
@@ -68,42 +68,61 @@ class EtudiantController extends UserController{
 				}
 
 			} else { // si ce n'est pas un QCM, on va directement chercher la réponse
-				$r = $this->correction($key, $value);
-			}
-
+			$r = $this->correction($key, $value);
 		}
 
 	}
 
+}
 
-	public function correctionQCM($reponses){
 
+public function correctionQCM($reponses){
+	$r = true;
+	foreach ($reponses as $key => $value) {
+			// valeur dans le post :
+			// 'QCM_qId:1_nbRep:4_rId:1_juste:1' => string 'off' (length=3)
+  		// 'QCM_qId:1_nbRep:4_rId:2_juste:0' => string 'on' (length=2)
+		$juste = explode(":",explode("_", $key)[4])[1];
+		if( ($juste == 1 && $value == 'off') || ($juste == 0 && $value == 'on') )
+			$r = false; 
 	}
+	if($r)
+		var_dump('QCM :true');
+	else
+		var_dump('QCM :false');
+	return $r;
+}
 
 
 	// retourne si la/les réponse(s) pour la question est/sont juste(s)
 	// retourne id question
 	// permettra l'insertion dans la table reponse choisie
-	public function correction($key, $value){
-		$explode = explode("_", $key);
-		$type = $explode[0];
+public function correction($key, $value){
+	$explode = explode("_", $key);
+	$type = $explode[0];
 
-		$r = true;
-		switch ($type) {
+	$r = true;
+	switch ($type) {
 
-			case 'QCU':
+		case 'QCU':
+		// 'QCU_qId:13' => string 'rId:18_juste:1' (length=14)
+		$r = (explode(":", $value)[2] == 1);
+		break;
+
+		case 'LIBRE':
+  	// 'LIBRE_qId:14_rep:Pythagore' => string 'Py' (length=2)
+		$r = (explode(":", $key)[2] == $value);
+		break;
+
+		case 'ASSIGNE':
 					# code...
-			break;
-
-			case 'LIBRE':
-					# code...
-			break;
-
-			case 'ASSIGNE':
-					# code...
-			break;
-		}
+		break;
 	}
+	if($r)
+		var_dump($type.' :true');
+	else
+		var_dump($type.' :false');
+}
 
 
 
--- templates/repondreQuestionnaireTemplate.php
+++ templates/repondreQuestionnaireTemplate.php
@@ -3,23 +3,18 @@
 	<?php 
 	$questions = $this->args['questions'];
 	$reponses = $this->args['reponses'];
-	$QidsEtRidsAssociees = array(); 
 	// pour la sauvegarde des elements du questionnaire
 	// permet de ne pas avoir à rechercher ces éléments lors de la correction
 	foreach ($questions as $q) {
 		$q_id = $q->id();
 		$reps_associees = array();
-		$repIds_as =array(); // pour sauvegarder les ids des reponses
 		foreach ($reponses as $r) {
 			if($r->idQuestion() == $q_id){
 				array_push($reps_associees, $r);
-				array_push($repIds_as, $r->id());
 			}
 		}
 		require('QetRTemplate.php');
-		$QidsEtRidsAssociees[$q->id()] = $repIds_as;
 	}
-	$_SESSION['QetR'] = $QidsEtRidsAssociees;
 	?>
 	<input type="submit" name="ValiderReponses">
 <!-- <?php //echo "'Valider".$this->args['idQu']."'" ?> -->
