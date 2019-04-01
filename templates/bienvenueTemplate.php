efa90641f55315b31cd8c58c09159ccc16f08342
Maxime <maxime.fuchs1@gmail.com>
2019-04-01 17:26:56 +0200

    compteur

--- templates/bienvenueTemplate.php
+++ templates/bienvenueTemplate.php
@@ -1,2 +1,35 @@
 <img class="displayed" src="assets/Fichier3.png" alt="sabres">
 <h1 style="text-align: center;">BIENVENUE</h1>
+
+
+<?php 
+$nbQuestions = count(Question::getList());
+$nbQuestionnaires = count(Questionnaire::getList());
+$nbUsers = count(User::getList());
+$nbReponse = count(Reponse::getList());
+
+
+ ?>
+
+<div class="container">
+  <div class="row">
+    <div class="col-sm-6 compteur">
+    	<h1><?php echo $nbUsers;?></h1>
+      <h3>utilisateurs</h3>
+    </div>
+    <div class="col-sm-6 compteur">
+    	<h1><?php echo $nbQuestionnaires; ?></h1>
+      <h3>questionnaires</h3>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-sm-6 compteur">
+    	<h1><?php echo $nbQuestions; ?></h1>
+      <h3>questions</h3>
+    </div>
+    <div class="col-sm-6 compteur">
+    	<h1><?php echo $nbReponse; ?></h1>
+      <h3>réponses</h3>
+    </div>
+  </div>
+</div>
--- templates/superUserMenuTemplate.php
+++ templates/superUserMenuTemplate.php
@@ -1,5 +1,15 @@
 <?php 
 $array = array('profil', 'users', 'questionnaires', 'questions', 'reponses', 'questionsEtreponses');
 require_once("fonctionAfficherMenu.php");
- ?>
+?>
 
+<li class="foot-menu">
+	<ul>
+		<a href="logout.php">
+			<li>Déconnexion</li>
+		</a>
+		<a href="index.php">
+			<li>Home</li>
+		</a>
+	</ul>
+</li>
