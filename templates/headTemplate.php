d397d90a6912da38daf3efd74f5458651fc6ff54
Maxime <maxime.fuchs1@gmail.com>
2019-04-02 17:06:06 +0200

    suppression de notre fichier bootstrap.css + utilisation de la version en ligne

--- templates/headTemplate.php
+++ templates/headTemplate.php
@@ -3,16 +3,11 @@
 <head>
 	<meta charset="utf-8">
 	<title>PROJET WEB</title>
-	<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> -->
-	<link rel="stylesheet" href="css/generalStyle.css" type="text/css"
-	media="screen" title="style1" charset="utf-8" />
-	<link rel="stylesheet" href="css/accueil.css" type="text/css"
-	media="screen" title="style1" charset="utf-8" />
-	<link rel="stylesheet" href="css/questionEtquestionnaires.css" type="text/css"
-	media="screen" title="style1" charset="utf-8" />
+	<link rel="stylesheet" href="css/generalStyle.css" type="text/css" media="screen" title="style1" charset="utf-8" />
+	<link rel="stylesheet" href="css/accueil.css" type="text/css" media="screen" title="style1" charset="utf-8" />
+	<link rel="stylesheet" href="css/questionEtquestionnaires.css" type="text/css" media="screen" title="style1" charset="utf-8" />
 	<script src="js/code.js"></script>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-  <!-- <link rel="stylesheet" href="css/bootstrap.css"> -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
